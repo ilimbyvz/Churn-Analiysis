09ae130add8937878f1cd64a70012d601f1e303f
İlim Betül Yavuz <ilimbyvz@gmail.com>
2019-10-26 16:07:33 +0300

    Update Churn.py

--- Churn.py
+++ Churn.py
@@ -29,7 +29,7 @@ warnings.filterwarnings('ignore')
 
 
 
-#Veriseti import edilir
+#dataset is imported 
 dataset = pd.read_csv('Churn_Modelling.csv')
 
 
@@ -38,54 +38,54 @@ for column in dataset.columns:
             continue
         dataset[column] = LabelEncoder().fit_transform(dataset[column])
 
-#Bağımsız değişken olan X:3.kolondan itibaren sona kadar alınır
-#Y değişkeni(tahmin) son kolon alınır.
+#X  that independent variable is taken from the 3rd column until the end 
+#Y variable(prediction) is taken last column
 X = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
 
 
-#Veriseti eğitim ve test olarak  bölünür
+#Data set is divided into testing and training.
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.33, random_state = 0)
 
-#callback tanımlanır.
+#Callback is defined.
 mycallbacks = EarlyStopping(monitor='loss', patience=2,verbose=1,restore_best_weights=True)
 
-#Özelliklerin Ölçeklenmesi(Scaling) yapılır
+#Scaling of features
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
 
 
-#3 tane yapay sinir ağı oluşturulur:giriş(1),gizli(1),çıkış(1)
+#3 artificial neural networks:input(1),hide(1),exit(1)
 model = Sequential()
 
-# giriş katmanı
+#input layer
 model.add(Dense(256,activation="relu", input_dim=10, kernel_initializer="uniform"))
 
-#gizli katman
+#hide layer
 model.add(Dense(128,activation="relu", kernel_initializer="uniform"))
 
-#çkış katmanı
+#exit layer
 model.add(Dense(1,activation="sigmoid",  kernel_initializer="uniform"))
 
 
-#sinir ağı modeli çalıştırılır
+#Neural network model is run
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics = ['accuracy'])
 
-#Ağ modeli eğitim setine uygulanır. 
+#Neural network applies training dataset
 history=model.fit(X_train, y_train,validation_split=0.2, batch_size=X_train.shape[0], epochs=200, callbacks=[mycallbacks], verbose=1)
 
-#sonuçlar tahmin edilir.
+#Result are predicted 
 y_pred = model.predict(X_test)
 y_pred = (y_pred > 0.5)
 
-# Confusion Matrix oluşturulur
+# Confusion Matrix is created
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
 
 
-#Modelin doğruluk fonksiyonu görselleştirmesi yapılır
+#Accuracy Function of Model is done visualization
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
 plt.title('Model accuracy')
@@ -95,7 +95,7 @@ plt.legend(['Train', 'Test'], loc='upper left')
 plt.show()
 plt.savefig("acc.png")
 
-#Modelin kayıp fonksiyonu göselleştirmesi yapılır
+#Lost Function of Model is done visualization
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
 plt.title('Model loss')
